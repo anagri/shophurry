a935ec9be0d6d08244393a66293924fd9febb5ee
Amiruddin Nagri <amir.nagri+git@gmail.com>
2013-03-17 11:48:19 +0530

    everything

---
+++ src/com/shophurry/SearchResultActivity.java
@@ -0,0 +1,11 @@
+package com.shophurry;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class SearchResultActivity extends Activity {
+    public void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+    }
+}
